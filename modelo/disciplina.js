9380e0175abe1efa9d84366a9cb3e4f0093928a5
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-08-29 15:21:50 -0300

    adicionando #2 (classe disciplina)

---
+++ modelo/disciplina.js
@@ -0,0 +1,23 @@
+class Disciplina {
+    constructor(id, nome) {
+        this._id = id
+        this._nome = nome
+        this.alunos = []
+    }
+
+    get id() {
+        return this._id
+    }
+
+    set id(novoId) {
+        this._id = novoId
+    }
+
+    get nome() {
+        return this._nome;
+    }
+
+    set nome(novoNome) {
+        this._nome = novoNome;
+    }
+}
